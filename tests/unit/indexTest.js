5ef28bd4460575d7d20265e7ca8a5b8dbe35dd6e
Michael Scherer <mail@scherermichael.net>
2018-02-19 00:01:07 +0100

    Feat: Wrap subtract method into class.

--- tests/unit/indexTest.js
+++ tests/unit/indexTest.js
@@ -2,11 +2,29 @@
 
 const assert = require('assertthat');
 
-const subtract = require('../../..lib');
+const MyMath = require('../../..lib');
 
-suite('subtract', () => {
-  test('subtracts two numbers', done => {
-    assert.that(subtract(10, 9)).is.equalTo(1);
+suite('MyMath', () => {
+  test('is a function.', done => {
+    assert.that(MyMath).is.ofType('function');
     done();
+  });
+
+  suite('subtract', () => {
+    let myMath;
+
+    setup(() => {
+      myMath = new MyMath();
+    });
+
+    test('is a function.', done => {
+      assert.that(myMath.subtract).is.ofType('function');
+      done();
+    });
+
+    test('subtracts two numbers.', done => {
+      assert.that(myMath.subtract(10, 9)).is.equalTo(1);
+      done();
+    });
   });
 });
